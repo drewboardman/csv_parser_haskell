e8fb9f3e6b1582946b53241ae3c476a8ad1e3826
Drew Boardman <drew.e.boardman@gmail.com>
2019-03-26 23:25:47 -0500

    Getting closer!!!!

--- src/CsvReader.hs
+++ src/CsvReader.hs
@@ -1,20 +1,30 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module CsvReader (test) where
 
+import Data.Functor.Identity (Identity)
 import qualified Data.ByteString.Lazy as BL
 import           Data.Csv
 import qualified Data.Vector          as V
 
-data Row = Row
-  { sectionName :: !String
-  , menuName    :: !String
-  , channelID   :: !Int
-  , userName    :: !String
+type family HKD f a where
+  HKD Identity a = a
+  HKD f a        = f a
+
+data Row f = Row
+  { sectionName :: HKD f String
+  , menuName    :: HKD f String
+  , channelID   :: HKD f Int
+  , userName    :: HKD f String
   }
 
-instance FromNamedRecord Row where
+type RegularRow = Row Identity
+type MaybeRow = Row Maybe
+
+instance FromNamedRecord (Row a) where
   parseNamedRecord namedR = Row
     <$> namedR .: "Section Name"
       <*> namedR .: "Menu Name"
@@ -26,5 +36,21 @@ test = do
   csvData <- BL.readFile "tmp/robots.csv"
   case decodeByName csvData of
     Left err -> putStrLn err
-    Right (_, vecRow) -> V.forM_ vecRow (\ row ->
-      putStrLn $ sectionName row ++ menuName row ++ show (channelID row) ++ userName row)
+    Right (_, vecRow) -> V.forM_ vecRow $ filterAndPrint . validate
+
+filterAndPrint :: MaybeRow -> IO ()
+filterAndPrint mRow = do
+  sName <- sectionName mRow
+  mName <- menuName mRow
+  uName <- userName mRow
+  case (channelID mRow) of
+    Just channel -> (putStrLn $ sName ++ mName ++ show (channel) ++ uName)
+    Nothing -> ()
+
+isGoodChannel :: Int -> Maybe Int
+isGoodChannel c = if c > 5 then Just c else Nothing
+
+validate :: RegularRow -> MaybeRow
+validate (Row sName mName cID uName) = myRow where
+  mChannelID = isGoodChannel cID
+  myRow = Row (Just sName) (Just mName) mChannelID (Just uName)
