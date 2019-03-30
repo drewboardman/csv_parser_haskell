95538576acc6b7a1c2669823640e8afb0b2e2db8
Drew Boardman <drew.e.boardman@gmail.com>
2019-03-29 19:18:11 -0500

    it works

--- src/SimpleParser.hs
+++ src/SimpleParser.hs
@@ -6,16 +6,39 @@ module SimpleParser (simple) where
 import qualified Data.ByteString.Lazy  as BL
 import           Data.Csv
 import           Data.Functor.Identity (Identity)
--- import           Control.Monad.Extra
 import qualified Data.Vector           as V
+
+type Rows = V.Vector SingleRow
+type SingleRow = (Int, String)
 
 simple :: IO ()
 simple = do
-  csvData <- BL.readFile "tmp/users.csv"
+  csvData <- BL.readFile "tmp/test.csv"
   case decode HasHeader csvData of
     Left err -> putStrLn err
-    Right vec -> V.forM_ vec (\(userCode :: Int, groupCode :: Int) ->
-      putStrLn $ show userCode ++ show groupCode)
+    Right rows -> handleRight rows
+
+handleRight :: Rows -> IO ()
+handleRight rows = printAndWrite $ correctRows rows
+
+correctRows :: Rows -> Rows
+correctRows rows = V.filter filterer rows
+
+filterer :: SingleRow -> Bool
+filterer (id, foo) = id > 10
+
+printAndWrite :: Rows -> IO ()
+printAndWrite rows = printAction *> writeAction where
+  printAction = print stringified
+  writeAction = write stringified
+  stringified = encode $ V.toList rows
+
+write :: BL.ByteString -> IO () 
+write string = BL.writeFile "tmp/parsed.csv" string
+
+-- handleRight row = V.forM_ row (\(userCode :: Int, groupCode :: Int) ->
+--   putStrLn $ show userCode ++ show groupCode)
+
 
 -- rowToPrint :: (Int, Int) -> IO (BL.ByteString)
 -- rowToPrint (userCode :: Int, groupCode :: Int) =
